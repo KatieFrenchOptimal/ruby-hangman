2d712be962d8a32286742caa9898758497622974
Katie French <katie.french@optimalworkshop.com>
2022-10-11 14:14:42 +1300

    Basic game working - able to guess secret word, win/lose, run out of lives

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,38 @@
 #!/usr/bin/env ruby
 
-randomWord = File.readlines("words.txt").sample
-puts randomWord
+gameInProgress = true
+
+lives = 9
+
+secretWord = File.readlines("words.txt").sample.upcase.chop
+puts secretWord
+board =  ["_"] * secretWord.size
+puts board.join(" ")
+
+while lives > 0 && board.include?("_")
+  puts "Please guess a letter:"
+  guess = gets.chomp.upcase
+  puts "You guessed: '#{guess}'"
+  puts "You have #{lives} remaining lives left."
+
+  if secretWord.include?(guess)
+    secretWord.chars.each_with_index do |character, index|
+      if character == guess
+        board[index] = character
+      end
+    end
+  else
+    lives -= 1
+    puts "The word did not include #{guess}"
+  end
+
+  p board.join(" ")
+end
+
+if board.join("") == secretWord
+  puts "You won"
+elsif lives <= 0
+  puts "You lost. The word was: #{secretWord}"
+end
+
+puts "The word was #{secretWord}"
