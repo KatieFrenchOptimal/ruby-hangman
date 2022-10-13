761b97e9338224f9e0faf19ec63e7d57e131c54d
Katie French <katie.french@optimalworkshop.com>
2022-10-14 10:17:32 +1300

    Simple game set up tests

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -1,11 +1,19 @@
 require "./hangman"
 
 describe Hangman do
-  let(:game) { Hangman.new() }
+  let(:game) { Hangman.new }
 
   context "on game start up" do
     it "user should have 9 lives" do
-      expect(game.lives).to eq(9)
+      expect(game.lives).to eq 9
+    end
+
+    it "should generate a secret word" do
+      expect(game.secret_word).to_not be_empty
+    end
+
+    it "board should have an underscore for every character of the secret word" do
+      expect(game.board.length).to eq game.secret_word.size
     end
   end
 end
